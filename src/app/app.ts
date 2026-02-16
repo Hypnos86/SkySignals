4526c37bd61b80530f51c66fe61eba50862ecd91
Kamil K. <kubiak.kamil23@gmail.com>
2026-02-16 20:54:20 +0100

    add service and style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WeatherApi } from './weather-api';
+import { Station } from './interface/station';
 
 @Component({
   selector: 'app-root',
@@ -12,22 +11,42 @@ import { WeatherApi } from './weather-api';
 export class App {
   protected readonly title = signal('sky-signals');
 
-  private weatherService = inject(WeatherApi);
+  // private weatherService = inject(WeatherApi); sposób na wstrzyknięcie serwisu
+
+  // poprawny sposób na wstrzyknięcie serwisu
+  constructor(private weatherService: WeatherApi) {}
 
   weatherData = signal<any[]>([]);
   isLoading = signal(true);
+  cities = signal<Station[]>([]);
 
   ngOnInit(): void {
+    // this.weatherService.getAllWeather().subscribe({
+    //   next: (data) => {
+    //     this.weatherData.set(data); //zapisywanie danych do weatherData czyli do sygnału
+    //     this.isLoading.set(false);
+    //     console.log('dane załadowane', this.weatherService);
+    //   },
+    //   error:(err) =>{
+    //     console.error("Błąd przy wczytywaniu danych:", err);
+    //     this.isLoading.set(false)
+    //   }
+    // });
+    this.loadCities();
+  }
+
+  loadCities() {
     this.weatherService.getAllWeather().subscribe({
       next: (data) => {
-        this.weatherData.set(data); //zapisywanie danych do weatherData czyli do sygnału
+        console.log('Moje dane', data);
+        this.cities.set(data);
         this.isLoading.set(false);
-        console.log('dane załadowane', this.weatherService);
       },
-      error:(err) =>{
-        console.error("Błąd przy wczytywaniu danych:", err);
-        this.isLoading.set(false)
-      }
+      error: (err) => {
+        console.error('Błąd przy wczytywaniu dannych', err);
+        this.cities.set([]);
+        this.isLoading.set(false);
+      },
     });
   }
 }
