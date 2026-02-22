6dec92cdc27216051930d252d8e383c222b312da
Kamil <kubiak.kamil23@gmail.com>
2026-02-22 21:08:27 +0100

    add link in baner in header

---
+++ src/app/core/interfaces/hydrologicalData-interface.ts
@@ -0,0 +1,18 @@
+export interface hydrologicalDataInterface {
+  id_stacji: string;
+  stacja: string;
+  rzeka: string;
+  wojewodztwo: string;
+  lon: number;
+  lat: number;
+  stan_wody: number;
+  stan_wody_data_pomiaru: string;
+  temperatura_wody: null;
+  temperatura_wody_data_pomiaru: null;
+  przelyw: number;
+  przeplyw_data: string;
+  zjawisko_lodowe: number;
+  zjawisko_lodowe_data_pomiaru: string;
+  zjawisko_zarastania: number;
+  zjawisko_zarastania_data_pomiaru: string;
+}
